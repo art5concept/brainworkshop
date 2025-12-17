884e48fc517e1bf5cb6c127b8bb6637b205b0e6a
felix_lara <larafelix2197@gmail.com>
2025-12-17 17:21:29 -0500

    Fix wrapper script to use local venv3 and handle execution path

--- brainworkshop_wrapper.sh
+++ brainworkshop_wrapper.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-cd /opt/brainworkshop
-exec python3 brainworkshop.pyw "$@"
+# Try to run from installed location, otherwise run from current directory
+if [ -d "/opt/brainworkshop" ]; then
+    cd /opt/brainworkshop
+fi
+
+# Check for local venv3
+if [ -f "venv3/bin/python" ]; then
+    exec venv3/bin/python brainworkshop.pyw "$@"
+elif [ -f "venv/bin/python3" ]; then
+    exec venv/bin/python3 brainworkshop.pyw "$@"
+else
+    exec python3 brainworkshop.pyw "$@"
+fi
